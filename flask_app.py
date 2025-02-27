69abe39765c716af26744c2baeb96e8c6eaca844
adityaka <adityaka@live.com>
2025-02-27 19:45:23 +0530

    changes for a reproduction and updated some keymanager code which will need more work

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,9 @@
-from flask import Flask, redirect, request
+from flask import Flask, redirect, Request, Response, make_response, send_file
 from time import sleep
 import config
+import io
+import git
+
 
 app = Flask(__name__,
             static_folder=config.MYAPP_CONFIG["static_directory"],
@@ -24,3 +27,22 @@ def redir(delay=3):
     sleep(delay)    
     return redirect('/static/html/heavyone.html', code=302)
 
+@app.route('/update/<key>')
+def update(key):
+    badKeyResponse = Response(status=404, response=b"Don't know what do you want me to do")
+    if not key:
+        return badKeyResponse
+
+@app.route('/repros/badcontentencoding', methods=['GET', 'POST'])
+def bad_content_encoding():
+    binary_data = b'This is some binary data.'
+    file_like_object = io.BytesIO(binary_data)
+    resp =  make_response(send_file(file_like_object, mimetype='application/octet-stream', as_attachment=True, download_name='bindata.bin'))
+    resp.headers.set('Content-Type', 'application/octet=stream')
+    resp.status_code = 200
+    return resp
+     
+     
+    
+    
+         
